a328bee9f487cf1d25c32d829c0ef97145295c0a
Alex Samson <samsonioanalexandru@gmail.com>
2022-04-27 12:12:48 +0300

    add mapDispatchToProps

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,14 @@
-import { applyMiddleware, compose, createStore } from "redux"
+import { applyMiddleware, compose, createStore } from "redux";
 
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant"
-import rootReducer from "./reducers"
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import rootReducer from "./reducers";
 
 export default function configureStore(initialState) {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose //add support for redux devtools
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ || compose; //add support for redux devtools
 
   return createStore(
     rootReducer,
     initialState,
     composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
-  )
+  );
 }
