834cc4738dbd2d897e9e6212c49b301f7b0f5fb8
Alex Samson <samsonioanalexandru@gmail.com>
2022-04-27 11:48:23 +0300

    remove debugger breakpoints

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -18,7 +18,6 @@ class CoursesPage extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    debugger;
     this.props.dispatch(courseActions.createCourse(this.state.course));
   };
 
@@ -47,7 +46,6 @@ CoursesPage.propTypes = {
 };
 
 function mapStateToProps(state) {
-  debugger;
   return {
     courses: state.courses,
   };
